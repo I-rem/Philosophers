fe696be0eaad244287872fa660d67b92c8d29f1e
İrem Ayşe Kayacıoğlu <72342267+I-rem@users.noreply.github.com>
2023-07-19 15:50:59 +0300

    Fix segfault

--- philo.c
+++ philo.c
@@ -12,33 +12,32 @@
 
 #include "philo.h"
 
-void    manager(t_philo *philo)
+void*   manager(void *philo)
 {
     long	die_time;
     int		has_eaten;
 	long	current_time;
 	long	time_since_last_eat;
-	
-    die_time = philo->die_time;
-    has_eaten = 0;
-    while (1) 
+
+	die_time = ((t_philo *)philo)->die_time;
+	has_eaten = 0;
+	while (1) 
 	{
-        usleep(100);
-        current_time = get_timestamp(philo);
-        time_since_last_eat = current_time - philo->last_eat_time;
- 	if (!(has_eaten) && time_since_last_eat > die_time) 
+		usleep(100);
+		current_time = get_timestamp(philo);
+		time_since_last_eat = current_time - ((t_philo *)philo)->last_eat_time;
+		if (!(has_eaten) && time_since_last_eat > die_time) 
 	{
-        print_philo_died(philo);
+        print_death(philo);
         has_eaten = 1;
     }
-        if (philo->is_alive &&  time_since_last_eat > die_time)
+        if (((t_philo *)philo)->is_alive &&  time_since_last_eat > die_time)
             print_death(philo);
         check_starve(philo, time_since_last_eat);
-        if (!philo->is_alive)
+        if (!((t_philo *)philo)->is_alive)
             break;
     }
     return (NULL);
-
 }
 
 void    check_starve(t_philo *philo, long time_since_last_eat)
@@ -50,21 +49,22 @@ void    check_starve(t_philo *philo, long time_since_last_eat)
         pthread_mutex_lock(&philo->table->finish_lock);
         done_eating = philo->table->done_eating;
         pthread_mutex_unlock(&philo->table->finish_lock);
-        if(done_eating < philo->table->philo_num - 1)
+        if(done_eating < philo->table->num_philos - 1)
             print_death(philo);
     }
 }
 
 void    check_must_eat_num(t_philo *philo, int must_eat_num)
 {
-    if (must_eat_num != -1 && philo->eat_count >= must_eat_num) //try to make this work with == must_eat_num 
+    int done_eating;
+    done_eating = 0;
+    if (must_eat_num != -1 && philo->eat_count == must_eat_num) //try to make this work with == must_eat_num 
     {
-        int done_eating;
         pthread_mutex_lock(&philo->table->finish_lock);
-        done_eating = philo->table-done_eating + 1;
+        done_eating = philo->table->done_eating + 1;
         pthread_mutex_unlock(&philo->table->finish_lock);
     }
-    if (done_eating == philo->table->philo_num)
+    if (done_eating == philo->table->num_philos)
     {
         pthread_mutex_lock(&philo->table->print_lock);
         if (!philo->table->has_dead)
@@ -75,10 +75,10 @@ void    check_must_eat_num(t_philo *philo, int must_eat_num)
 
 void    philo_sleep(t_philo *philo)
 {
-    print_message(philo, "is sleeping");
+    print_log(philo, "is sleeping");
     usleep(philo->sleep_time * 1000);
     pthread_mutex_lock(&philo->eat_count_lock);
-    philo->eat_count++;
+    philo->eat_count++; // !
     pthread_mutex_unlock(&philo->eat_count_lock);
 }
 
@@ -89,7 +89,7 @@ long    get_timestamp(t_philo *philo)
 
     gettimeofday(&current_time, NULL);
     timestamp = (current_time.tv_sec - philo->table->start_time.tv_sec) * 1000 +
-                (current_time.tv_usec - philo->table->start_time.u_sec) / 1000;
+                (current_time.tv_usec - philo->table->start_time.tv_usec) / 1000;
     return (timestamp);
 }
 
@@ -99,8 +99,8 @@ void    print_log(t_philo *philo, char *message)
     if (!philo->table->has_dead)
     {
         printf("%ld %d %s\n", get_timestamp(philo), philo->id, message);
-        //if (message[0] == 'd')
-        //    philo->table->has_dead=1;
+        if (message[0] == 'd') // !
+            philo->table->has_dead=1; // !
     }
     pthread_mutex_unlock(&philo->table->print_lock);
 }
@@ -122,7 +122,7 @@ void    free_all(t_table *table)
     int i;
 
     i = -1;
-    while (++i < table->philo_num)
+    while (++i < table->num_philos)
          pthread_mutex_destroy(&table->forks[i]);
     pthread_mutex_destroy(&table->print_lock);
     pthread_mutex_destroy(&table->finish_lock);
@@ -137,36 +137,36 @@ void    free_all(t_table *table)
 
 void    init_table(t_table *table, int philo_num, int must_eat_num)
 {
-    table->philo_num = philo_num;
+    table->num_philos = philo_num;
     table->must_eat_num = must_eat_num;
     table->has_dead = 0;
     table->done_eating = 0;
     gettimeofday(&table->start_time, NULL);
     table->philos = malloc(philo_num * sizeof(t_philo));
     table->forks = malloc(philo_num * sizeof(pthread_mutex_t));
-    table->thread =malloc(philo_num * sizeof(pthread_t));
+    table->threads =malloc(philo_num * sizeof(pthread_t));
     pthread_mutex_init(&table->print_lock, NULL);
     pthread_mutex_init(&table->finish_lock, NULL);
     memset(table->philos, 0, philo_num * sizeof(t_philo));
 }
 
-void    init_philos(int die_time, int eat_time, int sleep_time)
+void    init_philos(t_table *table, int die_time, int eat_time, int sleep_time)
 {
     int i;
 
     i = 0;
-    while(i < table->philo_num)
+    while(i < table->num_philos)
     {
         table->philos[i].id = i + 1;
-        table->philos[i].die_time = die_time
-        table->philos[i].eat_time = eat_time
-        table->philos[i].sleep_time = sleep_time
+        table->philos[i].die_time = die_time;
+        table->philos[i].eat_time = eat_time;
+        table->philos[i].sleep_time = sleep_time;
         table->philos[i].eat_count = 0;
         table->philos[i].is_alive = 1;
         table->philos[i].table = table;
         table->philos[i].left_fork = &table->forks[i];
-        table->philos[i].right_fork = &table->forks[(i + 1) % table->philo_num];
-        pthread_mutex_init(&table->philos[i].eat_count_lock, NULL)
+        table->philos[i].right_fork = &table->forks[(i + 1) % table->num_philos];
+        pthread_mutex_init(&table->philos[i].eat_count_lock, NULL);
         i++;
     }
 }
@@ -176,7 +176,7 @@ void    init_forks(t_table *table)
     int i;
 
     i = -1;
-    while (++i < table->philo_num)
+    while (++i < table->num_philos)
         pthread_mutex_init(&table->forks[i], NULL);
 }
 
@@ -189,12 +189,12 @@ void    pick_forks(t_philo *philo)
     right_fork = philo->right_fork;
     if (philo->id % 2 == 0)
     {
-        ptthread_mutex_lock(right_fork);
-        ptthread_mutex_lock(left_fork);
+        pthread_mutex_lock(right_fork);
+        pthread_mutex_lock(left_fork);
     }
     else
     {
-        ptthread_mutex_lock(left_fork);
+        pthread_mutex_lock(left_fork);
         pthread_mutex_lock(right_fork);
     }
 }
@@ -206,26 +206,28 @@ void    eat(t_philo *philo)
     if (!philo->table->has_dead)
     {
         print_log(philo, "is eating");
-        philo->last_eat_time = get_timestamp(philo);
-        usleep(philo->time_to_eat * 1000);
-    )
+        philo->eat_count += 1;
+		philo->last_eat_time = get_timestamp(philo);
+        usleep(philo->eat_time * 1000);
+
+    }
     pthread_mutex_unlock(philo->left_fork);
     pthread_mutex_unlock(philo->right_fork);
 }
 
-void    start_routine(t_philo   *philo)
+void*   start_routine(void   *philo)
 {
     pthread_t   manager_thread;
 
-    pthread_create(&manager, NULL, manager, philo);
+    pthread_create(&manager_thread, NULL, manager, philo);
     pthread_detach(manager_thread);
-    while (!philo->table->has_dead)
+    while (!((t_philo *)philo)->table->has_dead)
     {
-        check_must_eat_num(philo, must_eat_num);
+        check_must_eat_num(philo, ((t_philo *)philo)->table->must_eat_num);
         eat(philo);
         philo_sleep(philo);
     }
-    return(NULL);
+    return (NULL);
 }
 
 int ft_atoi(const char *str)
@@ -233,41 +235,27 @@ int ft_atoi(const char *str)
     long long result;
 
     result = 0;
-    while (*str == 32 || (*str >= 9 && *str <= 13))
+    while (*str == ' ' || (*str >= 9 && *str <= 13))
         str++;
     if (*str == '+')
         str++;
     while (*str >= '0' && *str <= '9')
     {
-        result = result * 10 + *str - 48;
+        result = result * 10 + *str - '0';
         str++;
         if (result > 2147483647)
-            result = -1;
+            return(-1);
     }
+	if (*str != '\0')
+		result = -1;
     return (result);
 }
 
 int arg_check(char **argv, int argc)
 {
-    int i;
-    int j;
-
-    if (argc < 5 || argc > 6)
+	(void)argv;	
+    if (argc != 5 && argc != 6)
         return (0);
-    i = 0;
-    while (argv[++i] != NULL)
-    {
-        j = -1;
-        while (argv[i][++j] != '\0')
-        {
-            if (argv[i][j] == '+' && !(argv[i][j + 1] <= '9' && argv[i][j + 1] >= '0'))
-                return (0);
-            else if (argv[i][j] == '+' && (j != 0 && argv[i][j - 1] != ' '))
-                return (0);
-            else if (argv[i][j] != ' ' && argv[i][j] != '+' && (argv[i][j] < '0' || argv[i][j] > '9'))
-                return (0);
-        }
-    }
     return (1);
 }
 
@@ -278,8 +266,8 @@ int	main(int argc, char **argv)
     int     i;
 
 	must_eat_num = -1;
-	if (!arg_check(argv, argc) || ft_atoi(argv[1]) == -1 || ft_atoi(argv[2]) == -1 || ft_atoi(argv[3]) == -1
-		|| ft_atoi(argv[4]) || (argc == 6 && ft_atoi(argv[5]) == -1)) 
+	if (!arg_check(argv, argc)	|| ft_atoi(argv[1]) == -1 || ft_atoi(argv[2]) == -1 || ft_atoi(argv[3]) == -1
+		|| ft_atoi(argv[4]) == -1 || (argc == 6 && ft_atoi(argv[5]) == -1)) 
 	{
         write(2, "Enter valid arguments!\n", 23);
         return (1);
@@ -290,10 +278,10 @@ int	main(int argc, char **argv)
     init_forks(&table);
     init_philos(&table,ft_atoi(argv[2]), ft_atoi(argv[3]), ft_atoi(argv[4]));
     i = -1;
-    while(++i < table->philo_num)
-        pthread_create(&table->threads, NULL, start_routine, &table->philos[i]);
+    while(++i < table.num_philos)
+        pthread_create(table.threads, NULL, start_routine, &table.philos[i]);
     i = -1;
-     while(++i < table->philo_num)
-        pthread_join(table->threads, NULL);
+     while(++i < table.num_philos)
+        pthread_join(*table.threads, NULL);
     free_all(&table);
 }
--- philo.h
+++ philo.h
@@ -32,38 +32,38 @@ typedef struct s_philo
 	int				eat_count;
 	int				is_alive;
 	pthread_mutex_t	*right_fork;
-	pthread_mutex_t	*left_fork
-	pthread_mutex_t eat_count_lock;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	eat_count_lock;
 	t_table	*table;
 }	t_philo;
 
-typdef struct	s_table
+typedef struct s_table
 {
-	int		num_philos;
-	t_philo	*philos;
-	int		must_eat_num;
-	int		has_dead;
-	int		done_eating;
+	int				num_philos;
+	t_philo			*philos;
+	int				must_eat_num;
+	int				has_dead;
+	int				done_eating;
 	pthread_mutex_t	*forks;
-	pthread_mutex_t	*print_lock;
-	pthread_mutex_t	*finish_lock;
-	pthread_t	*threads;
-	struct	timeval	start_time;
+	pthread_mutex_t	print_lock;
+	pthread_mutex_t	finish_lock;
+	pthread_t		*threads;
+	struct timeval	start_time;
 }	t_table;
 
-void    free_all(t_table *table);
-void    init_table(t_table *table, int philo_num, int must_eat_num);
-void    init_philos(int die_time, int eat_time, int sleep_time)
-void    init_forks(t_table *table);
-int ft_atoi(const char *str);
-int arg_check(char **argv, int argc);
-void    create_manager(t_philo *philo);
-void    manager(t_philo *philo);
-void    check_death(t_philo *philo, long last_eat_time, long die_time);
-void    check_starve(t_philo *philo, long time_since_last_eat);
-void    check_must_eat_num(t_philo *philo, int must_eat_num);
-void    philo_sleep(t_philo *philo);
-long    get_timestamp(t_philo *philo);
-void    print_log(t_philo *philo, char *message);
-void    print_death(t_philo *philo)
+void*	manager(void *philo);
+void	check_starve(t_philo *philo, long time_since_last_eat);
+void	check_must_eat_num(t_philo *philo, int must_eat_num);
+void	philo_sleep(t_philo *philo);
+long	get_timestamp(t_philo *philo);
+void	print_log(t_philo *philo, char *message);
+void	print_death(t_philo *philo);
+void	free_all(t_table *table);
+void	init_table(t_table *table, int philo_num, int must_eat_num);
+void	init_philos(t_table *table, int die_time, int eat_time, int sleep_time);
+void	init_forks(t_table *table);
+void	eat(t_philo *philo);
+void*	start_routine(void *philo);
+int		ft_atoi(const char *str);
+int		arg_check(char **argv, int argc);
 #endif
