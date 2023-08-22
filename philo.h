3c5add99c7a4d6d715e51a565d803c493cb75e95
İrem Ayşe Kayacıoğlu <72342267+I-rem@users.noreply.github.com>
2023-08-22 23:31:54 +0300

    Update philo.h

--- philo.h
+++ philo.h
@@ -1,60 +1,49 @@
 #ifndef PHILO_H
-# define PHILO_H
-# include <string.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <sys/time.h>
-# include <pthread.h>
+#define PHILO_H
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <unistd.h>
 
-typedef struct s_philo	t_philo;
-typedef struct s_table	t_table;
+typedef struct s_philo t_philo;
+typedef struct s_table t_table;
 
-typedef struct    s_philo
-{
-    int            id;
-    long            last_eat_time;
-    int            eat_count;
-    int            is_alive;
-    int            die_time;
-    int            eat_time;
-    int            sleep_time;
-    t_table        *table;
-    pthread_mutex_t *left_fork;
-    pthread_mutex_t *right_fork;
-    pthread_mutex_t eat_count_lock;
-    pthread_mutex_t eat_lock;
-}                t_philo;
+typedef struct s_philo {
+  int id;
+  long last_eat_time;
+  int eat_count;
+  int is_alive;
+  int die_time;
+  int eat_time;
+  int sleep_time;
+  t_table *table;
+  pthread_mutex_t *left_fork;
+  pthread_mutex_t *right_fork;
+  pthread_mutex_t eat_count_lock;
+  pthread_mutex_t eat_lock;
+} t_philo;
 
+typedef struct s_table {
+  int num_philos;
+  t_philo *philos;
+  int must_eat_num;
+  int has_dead;
+  int done_eating;
+  pthread_mutex_t *forks;
+  pthread_mutex_t print_lock;
+  pthread_mutex_t finish_lock;
+  pthread_t *threads;
+  struct timeval start_time;
+} t_table;
 
-typedef struct s_table
-{
-	int				num_philos;
-	t_philo			*philos;
-	int				must_eat_num;
-	int				has_dead;
-	int				done_eating;
-	pthread_mutex_t	*forks;
-	pthread_mutex_t	print_lock;
-	pthread_mutex_t	finish_lock;
-	pthread_t		*threads;
-	struct timeval	start_time;
-}	t_table;
-
-void	*manager(void *philo);
-void	check_starve(t_philo *philo, long time_since_last_eat);
-void	check_must_eat_num(t_philo *philo, int must_eat_num);
-void	philo_sleep(t_philo *philo);
-long	get_timestamp(t_philo *philo);
-void	print_log(t_philo *philo, char *message);
-void	print_death(t_philo *philo);
-void	free_all(t_table *table);
-void	init_table(t_table *table, int philo_num, int must_eat_num);
-void	init_philos(t_table *table, int die_time, int eat_time, int sleep_time);
-void	init_forks(t_table *table);
-void	eat(t_philo *philo);
-void	*start_routine(void *philo);
-int		ft_atoi(const char *str);
-int		arg_check(char **argv, int argc);
-void	pick_forks(t_philo *philo);
+void init_table(t_table *table, int philo_num, int must_eat_num);
+void init_philos(t_table *table, int die_time, int eat_time, int sleep_time);
+void init_forks(t_table *table);
+long get_timestamp(t_philo *philo);
+void print_log(t_philo *philo, char *s);
+void eat(t_philo *philo, int type);
+int is_dead(t_philo *philo);
+void philo_sleep(int type, t_philo *philo);
 #endif
