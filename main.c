e9f122e8e120adc27fa28a2819e5d3430a514ec5
İrem Ayşe Kayacıoğlu <72342267+I-rem@users.noreply.github.com>
2023-08-22 23:31:06 +0300

    Update and rename philo to main.c

---
+++ main.c
@@ -0,0 +1,112 @@
+#include "philo.h"
+
+void philo_sleep(int type, t_philo *philo)
+{
+  if (type == 1)
+  {
+    print_log(philo, "is sleeping");
+     if (philo->sleep_time >= philo->die_time)
+      usleep(1000*(philo->die_time));
+    else
+      usleep(philo->sleep_time * 1000);
+  }
+  else
+  {
+    if (philo->eat_time >= philo->die_time)
+      usleep(1000*(philo->die_time));
+    else
+      usleep(philo->eat_time * 1000);
+  }
+}
+
+void *start_routine(void *v_philo) {
+  t_philo *philo;
+
+  philo = (t_philo *)v_philo;
+  //print_log(philo, "is thinking");
+  while (!philo->table->has_dead && !is_dead(philo)
+  && !((philo->eat_count == philo->table->must_eat_num) && philo->table->must_eat_num != 0))
+  {
+    print_log(philo, "is thinking");
+    if (philo->id % 2 == 1)
+      eat(philo, 1);
+    else
+      eat(philo, 2);
+    if(!philo->table->has_dead && !is_dead(philo)
+  && !((philo->eat_count == philo->table->must_eat_num) && philo->table->must_eat_num != 0))
+    philo_sleep(1, philo);
+  }
+  return (NULL);
+}
+
+void free_all(t_table *table) {
+  int i;
+
+  i = -1;
+  while (++i < table->num_philos) {
+    pthread_mutex_destroy(&table->forks[i]);
+    pthread_mutex_destroy(&table->philos[i].eat_count_lock);
+    pthread_mutex_destroy(&table->philos[i].eat_lock);
+  }
+  pthread_mutex_destroy(&table->print_lock);
+  pthread_mutex_destroy(&table->finish_lock);
+  if (table->philos != NULL)
+    free(table->philos);
+  if (table->forks != NULL)
+    free(table->forks);
+  if (table->threads != NULL)
+    free(table->threads);
+}
+
+int ft_atoi(const char *str) {
+  long long result;
+
+  result = 0;
+  while (*str == ' ' || (*str >= 9 && *str <= 13))
+    str++;
+  if (*str == '+')
+    str++;
+  while (*str >= '0' && *str <= '9') {
+    result = result * 10 + *str - '0';
+    str++;
+    if (result > 2147483647)
+      return (-1);
+  }
+  if (*str != '\0')
+    result = -1;
+  return (result);
+}
+
+int arg_check(char **argv, int argc) {
+  (void)argv;
+  if (argc != 5 && argc != 6)
+    return (0);
+  return (1);
+}
+
+int main(int argc, char **argv) {
+  t_table table;
+  int must_eat_num;
+  int i;
+
+  must_eat_num = -1;
+  if (!arg_check(argv, argc) || ft_atoi(argv[1]) == -1 ||
+      ft_atoi(argv[2]) == -1 || ft_atoi(argv[3]) == -1 ||
+      ft_atoi(argv[4]) == -1 || (argc == 6 && ft_atoi(argv[5]) == -1)) {
+      write(2, "Enter valid arguments!\n", 23);
+    return (1);
+  }
+  if (argc == 6)
+    must_eat_num = ft_atoi(argv[5]);
+  init_table(&table, ft_atoi(argv[1]), must_eat_num);
+  init_forks(&table);
+  init_philos(&table, ft_atoi(argv[2]), ft_atoi(argv[3]), ft_atoi(argv[4]));
+  i = -1;
+  while (++i < table.num_philos)
+    pthread_create(&table.threads[i], NULL, start_routine, &table.philos[i]);
+  i = -1;
+  while (++i < table.num_philos)
+    pthread_join(table.threads[i], NULL);
+  free_all(&table);
+  return (0);
+}
